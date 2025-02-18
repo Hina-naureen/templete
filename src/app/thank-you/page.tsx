e480e23668732e725d9ad684be116a354fce5fa2
Hina-naureen <hina.naureen0102@gmail.com>
2025-02-19 04:20:02 +0500

    Update page.tsx

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
-const ThankYou = () => {
+const ThankYouPage = () => {
   const router = useRouter();
   const [paymentMethod, setPaymentMethod] = useState<string | null>(null);
 
@@ -53,4 +53,4 @@ const ThankYou = () => {
   );
 };
 
-export default ThankYou;
+export default ThankYouPage;
